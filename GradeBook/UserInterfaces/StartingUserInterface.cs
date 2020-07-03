c286bebe7ce3927aa53ae075502bfe54a6a89204
rwood94 <35955341+rwood94@users.noreply.github.com>
2020-07-03 17:49:36 +0100

    Completed Module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -54,7 +54,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[3].ToLower();
+            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
 
             if (type == "standard")
